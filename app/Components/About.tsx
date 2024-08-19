9152844e89c0d00aca9f2f622da68cc299d17208
Sirajudeen K.B <kbsirajudeen.cse@gmail.com>
2024-08-19 06:29:36 +0530

    Completed about section and steps section.

--- app/Components/About.tsx
+++ app/Components/About.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import interior from "../../public/images/interior-2.jpg";
 
+
 const About = () => {
   return (
     <div className="px-6 md:px-12 lg:px-24 lg:pt-24 pt-8 md:pt-16">
@@ -14,7 +15,8 @@ const About = () => {
       <div className="container mx-auto py-8 md:py-12 lg:py-16">
         <div className="flex flex-col lg:flex-row items-center lg:items-start">
           {/* Left container for the image */}
-          <div className="w-full lg:w-1/2 mb-8 lg:mb-0 lg:pr-8 animate-slide-right">
+
+          <div className="w-full lg:w-1/2 mb-8 lg:mb-0 lg:pr-8 ">
             <div className="relative w-full pb-[75%]">
               {" "}
               {/* 4:3 aspect ratio */}
@@ -28,7 +30,7 @@ const About = () => {
           </div>
 
           {/* Right container for text content */}
-          <div className="w-full lg:w-1/2 flex flex-col justify-start animate-slide-left">
+          <div className="w-full lg:w-1/2 flex flex-col justify-start ">
             <div className="text-center lg:text-left mb-6">
               <h3 className="text-lg sm:text-xl md:text-2xl font-medium text-left text-[#1F1F1F]">
                 Welcome to
@@ -56,6 +58,85 @@ const About = () => {
             <button className="px-6 py-3 bg-[#1F1F1F] text-white rounded hover:bg-[#444444] text-sm sm:text-base md:text-lg w-fit mx-auto lg:mx-0">
               Know More
             </button>
+          </div>
+        </div>
+      </div>
+      {/* Steps Section */}
+      <div className="container mx-auto py-6 md:py-12 lg:py-16">
+        <div className="flex flex-col lg:flex-row items-stretch">
+          <div className="w-full lg:w-1/2 mb-8 lg:mb-0 lg:pr-8 flex flex-col justify-between">
+            <div className="space-y-10">
+              <h2 className="text-3xl lg:text-5xl md:text-4xl font-semibold mb-8 text-[#1F1F1F]">
+                Designing Your Dream in Three Simple Steps
+              </h2>
+              <div className="flex items-start space-x-4">
+                <div className="flex-shrink-0">
+                  <img
+                    src="/assets/Step1.svg"
+                    alt="Start Project Icon"
+                    className="w-12 h-12"
+                  />
+                </div>
+                <div className="flex flex-col gap-y-4">
+                  <h3 className="text-xl font-semibold text-[#1F1F1F]">
+                    Start Project
+                  </h3>
+                  <p className="text-[#545454] text-sm sm:text-base md:text-lg">
+                    Embark on your design adventure by initiating your project.
+                    Share your vision and set the stage for a bespoke design
+                    experience.
+                  </p>
+                </div>
+              </div>
+              <div className="flex items-start space-x-4">
+                <div className="flex-shrink-0">
+                  <img
+                    src="/assets/Step2.svg"
+                    alt="Craft Icon"
+                    className="w-12 h-12"
+                  />
+                </div>
+                <div className="flex flex-col gap-y-4">
+                  <h3 className="text-xl font-semibold text-[#1F1F1F]">
+                    Craft
+                  </h3>
+                  <p className="text-[#545454] text-sm sm:text-base md:text-lg">
+                    Collaborate closely to achieve design excellence refining
+                    your vision and crafting brilliance into every aspect of
+                    your space.
+                  </p>
+                </div>
+              </div>
+              <div className="flex items-start space-x-4">
+                <div className="flex-shrink-0">
+                  <img
+                    src="/assets/Step3.svg"
+                    alt="Execute Icon"
+                    className="w-12 h-12"
+                  />
+                </div>
+                <div className="flex flex-col gap-y-4">
+                  <h3 className="text-xl font-semibold text-[#1F1F1F]">
+                    Execute
+                  </h3>
+                  <p className="text-[#545454] text-sm sm:text-base md:text-lg">
+                    Witness your vision becoming a reality as we execute the
+                    design plan with precision. Celebrate the joy of your newly
+                    transformed space.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-full lg:w-1/2 flex flex-col justify-start">
+            <div className="relative w-full pb-[75%]">
+              <Image
+                src={interior}
+                alt="About Us"
+                layout="fill"
+                className="rounded-lg object-cover"
+              />
+            </div>
           </div>
         </div>
       </div>

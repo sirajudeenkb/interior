85aefaf040a3064e87e099c82704f03fccdb6772
Sirajudeen K.B <kbsirajudeen.cse@gmail.com>
2024-08-19 06:28:46 +0530

    Completed Navbar and Half Hero.

---
+++ app/Components/Hero.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image';
+import React from 'react';
+import interior from '../../public/images/interior-1.jpg'
+
+const Hero = () => {
+  return (
+    <div className="p-6 md:p-12 lg:p-24 flex flex-col-reverse md:flex-row">
+      <div className="flex-1 flex flex-col items-start space-y-6 md:space-y-8 lg:space-y-10 md:mr-8 mt-6 md:mt-0">
+        <h1 className="font-medium text-5xl md:text-7xl lg:text-8xl text-[#1F1F1F]">
+          Interior Design
+        </h1>
+        <p className="text-[#545454] text-base md:text-lg lg:text-xl">
+          Step into a world where the art of Interior Design is meticulously
+          crafted to bring together timeless elegance and cutting-edge modern
+          Innovation, allowing you to transform your living spaces into the
+          epitome of luxury and sophistication.
+        </p>
+        <button className="w-full md:w-auto bg-[#1F1F1F] text-white text-sm md:text-base font-medium py-3 md:py-4 px-8 md:px-12 rounded hover:bg-[#444444]">
+          Start project
+        </button>
+      </div>
+      <div className="flex-shrink-0 w-full md:w-auto">
+        <Image
+          className="w-full md:w-auto h-64 md:h-80 lg:h-96 object-cover"
+          src={interior}
+          alt="Interior Design"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Hero
---
+++ app/Components/Navbar.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import React from "react";
+import Logo from "../../public/assets/Logo.svg";
+import Menu from "../../public/assets/Menu.svg";
+
+const navLinks = [
+  { name: "Home" },
+  { name: "About Us" },
+  { name: "Services" },
+  { name: "Our Projects" },
+];
+
+const Navbar = () => {
+  return (
+    <nav className="flex w-full items-center justify-between px-[20px] py-[15px] lg:container lg:mx-auto lg:px-20">
+      <div>
+        <span className="lg:block md:block hidden text-xl text-[#1F1F1F] font-semibold">
+          Br Interiors
+        </span>
+        <Image className="lg:hidden md:hidden sm:block" src={Logo} alt="Logo" />
+      </div>
+
+      <div className="hidden lg:flex md:flex gap-x-[50px]">
+        {navLinks.map((item, index) => (
+          <p className="text-[#1F1F1F] font-medium" key={index}>
+            {item.name}
+          </p>
+        ))}
+      </div>
+
+      <button className="bg-[#1F1F1F] text-white text-sm font-semibold py-3 px-6 rounded hover:bg-[#444444] hidden lg:block md:block">
+        Contact Us
+      </button>
+
+      <Image className="lg:hidden md:hidden block" src={Menu} alt="Menu" />
+    </nav>
+  );
+};
+
+export default Navbar;

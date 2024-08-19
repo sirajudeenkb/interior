5da2b39b96e8010b9cf5e3548a3e1e44d0947b5d
Sirajudeen K.B <kbsirajudeen.cse@gmail.com>
2024-08-19 06:31:52 +0530

    Updated hero section contents.

--- app/Components/Hero.tsx
+++ app/Components/Hero.tsx
@@ -37,14 +37,15 @@ const Hero = () => {
   return (
     <div className="px-6 md:px-12 lg:px-24 lg:pt-24 pt-6 md:pt-12 flex flex-col-reverse md:flex-row overflow-hidden">
       <div className="flex-1 flex flex-col items-start space-y-6 md:space-y-8 lg:space-y-10 md:mr-8 mt-6 md:mt-0 animate-slide-right">
-        <h1 className="font-medium text-5xl md:text-7xl lg:text-8xl text-[#1F1F1F]">
-          Interior Design
+        <h1 className="font-medium text-2xl md:text-4xl lg:text-6xl text-[#1F1F1F]">
+          Transform Your Space with Expert Interior Design
         </h1>
         <p className="text-[#545454] text-base md:text-lg lg:text-xl">
-          Step into a world where the art of Interior Design is meticulously
-          crafted to bring together timeless elegance and cutting-edge modern
-          Innovation, allowing you to transform your living spaces into the
-          epitome of luxury and sophistication.
+          Discover the perfect blend of timeless elegance and contemporary
+          innovation at Br Interior. We craft luxurious,
+          sophisticated living spaces that reflect your unique style. Let our
+          designers turn your home into a stunning masterpiece
+          you'll love for years to come.
         </p>
         <button className="w-full md:w-auto bg-[#1F1F1F] text-white text-sm md:text-base font-medium py-3 md:py-4 px-8 md:px-12 rounded hover:bg-[#444444]">
           Start project

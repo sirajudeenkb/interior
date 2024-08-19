d5109c131b4fb404f67e9ec486d3a576e8c73045
Sirajudeen K.B <kbsirajudeen.cse@gmail.com>
2024-08-19 06:30:50 +0530

    Completed about page.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,6 +5,7 @@ import AboutImage from "../../public/images/About.jpg"; // Ensure the image path
 import about from "../../public/images/about-us.jpg";
 import mission from "../../public/images/mission.jpg";
 import vision from "../../public/images/vision.jpg";
+import section from "../../public/images/Why-us.jpg";
 import Image from "next/image";
 import { useInView } from "react-intersection-observer";
 import Link from "next/link";
@@ -23,6 +24,11 @@ const About: React.FC = () => {
    });
 
    const { ref: visionRef, inView: visionInView } = useInView({
+     triggerOnce: true,
+     threshold: 0.2,
+   });
+
+   const { ref: stepsRef, inView: stepsInView } = useInView({
      triggerOnce: true,
      threshold: 0.2,
    });
@@ -129,7 +135,7 @@ const About: React.FC = () => {
         </div>
 
         {/* Content */}
-        <div className="relative z-10 flex flex-col md:flex-row justify-center gap-8 md:gap-12 lg:gap-16 max-w-full py-12 px-4 sm:px-8 md:px-12 lg:px-12">
+        <div className="relative z-10 flex flex-col md:flex-row justify-center gap-8 md:gap-12 lg:gap-16 max-w-full sm:p-8 md:p-12 lg:p-16">
           <div
             ref={missionRef}
             className={`flex flex-col items-center w-full md:w-1/2 lg:w-2/5 mb-12 md:mb-0 transition-opacity duration-[2500ms] ${
@@ -178,6 +184,118 @@ const About: React.FC = () => {
               setting new standards of excellence in the industry. Narasimha –
               The Top Interior Decorators
             </p>
+          </div>
+        </div>
+      </div>
+      <div className="relative p-8 md:p-12 lg:p-28">
+        <div className="absolute inset-0 overflow-hidden opacity-10">
+          <svg
+            id="visual"
+            viewBox="0 0 1577 620"
+            width="100%"
+            height="100%"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            version="1.1"
+            className="w-full h-full object-cover"
+          >
+            <rect x="0" y="0" width="1577" height="620" fill="#ffffff"></rect>
+            <g transform="translate(1572.1682739191738 484.46670565966974)">
+              <path
+                d="M380.4 -441.8C511.7 -343.2 649.8 -241 692.1 -106.4C734.5 28.2 681.2 195.4 602.4 362.8C523.6 530.2 419.3 697.8 248.4 819.5C77.5 941.1 -160.1 1016.7 -308.3 927.8C-456.5 838.8 -515.3 585.1 -575.6 380.8C-635.9 176.4 -697.6 21.2 -697.4 -147.9C-697.3 -317 -635.3 -500.2 -508.8 -599.7C-382.2 -699.2 -191.1 -715.1 -33.3 -675.4C124.5 -635.8 249.1 -540.5 380.4 -441.8"
+                fill="#ffa48f"
+              ></path>
+            </g>
+          </svg>
+        </div>
+        <div className="relative z-10 container mx-auto">
+          <div className="flex flex-col lg:flex-row items-stretch">
+            <div
+              ref={stepsRef}
+              className={`w-full lg:w-1/2 mb-8 lg:mb-0 lg:pr-8 flex flex-col justify-between transition-opacity duration-[2500ms] ${
+                stepsInView ? "animate-slide-right opacity-100" : "opacity-0"
+              }`}
+            >
+              <div className="space-y-10">
+                <h2 className="text-3xl md:text-4xl lg:text-5xl font-semibold mb-8 text-[#1F1F1F]">
+                  Why Choose Us?
+                </h2>
+                <p className="text-lg md:text-xl text-left text-[#1F1F1F] mb-8">
+                  Discover the Top 5 Reasons to Partner with Us!
+                </p>
+                <ul className="list-decimal ml-4 space-y-4 text-[#1F1F1F]">
+                  <li>
+                    <span className="font-bold">Expertise:</span>
+                    <span className="text-[#545454]">
+                      {" "}
+                      Benefit from our seasoned professionals with years of
+                      industry experience, ensuring top-notch design solutions
+                      tailored to your needs.
+                    </span>
+                  </li>
+                  <li>
+                    <span className="font-bold">Affordability:</span>
+                    <span className="text-[#545454]">
+                      {" "}
+                      Enjoy our budget-friendly options without compromising on
+                      quality or style, making exceptional design accessible to
+                      all.
+                    </span>
+                  </li>
+                  <li>
+                    <span className="font-bold">Personalization:</span>
+                    <span className="text-[#545454]">
+                      {" "}
+                      Experience our great personalized attention and customized
+                      designs that reflect your unique preferences and
+                      lifestyle.
+                    </span>
+                  </li>
+                  <li>
+                    <span className="font-bold">Timely Delivery:</span>
+                    <span className="text-[#545454]">
+                      {" "}
+                      It counts on us for efficient project management and
+                      timely completion, ensuring your vision becomes reality
+                      right on schedule.
+                    </span>
+                  </li>
+                  <li>
+                    <span className="font-bold">Customer Satisfaction:</span>
+                    <span className="text-[#545454]">
+                      {" "}
+                      Our responsible commitment to your satisfaction means
+                      transparent communication, attention to detail, and a
+                      seamless, stress-free design journey from start to finish.
+                    </span>
+                  </li>
+                </ul>
+                <div className="text-center lg:text-left mt-8">
+                  <p className="text-xl text-[#1F1F1F] font-semibold mb-4">
+                    Still Waiting?
+                  </p>
+                  <button className="px-6 py-3 bg-[#1F1F1F] text-white rounded hover:bg-[#444444] text-sm sm:text-base md:text-lg w-fit mx-auto lg:mx-0">
+                    <Link href="/contact">Contact Us</Link>
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div
+              ref={stepsRef}
+              className={`w-full lg:w-1/2 flex justify-center lg:justify-start transition-opacity duration-[2500ms] ${
+                stepsInView ? "animate-slide-left opacity-100" : "opacity-0"
+              }`}
+            >
+              <div className="relative w-full pb-[75%] rounded-lg overflow-hidden">
+                <Image
+                  src={section}
+                  alt="About Us"
+                  layout="fill"
+                  objectFit="cover"
+                  className="transition-transform duration-300 hover:scale-105"
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
